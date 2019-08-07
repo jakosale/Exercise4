c71cda9feb86c471783d57a13e2175b4d9f17169
jakosale <52512105+jakosale@users.noreply.github.com>
2019-08-07 13:48:31 -0400

    Add files via upload

---
+++ assets/data/schedule.ts
@@ -0,0 +1,12 @@
+import {BusSchedule} from '../../app/busschedule';
+
+export const MYBUS: BusSchedule []= [
+    {
+    route:'Mount Pleasant',
+    timeLeave:'2:00 PM',
+    timeArrive:'3:00 PM'},
+    {
+    route:'Sheridan College',
+    timeLeave:'8:00 AM',
+    timeArrive:'9:00 AM'}
+];
